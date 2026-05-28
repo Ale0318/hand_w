c79cc921d8c9481fbfe9302a4bd7f098bb4c77a3
Ale0318 <alejafonsecas0318@gmail.com>
2026-05-27 19:18:25 -0500

    Update App.py

--- App.py
+++ App.py
@@ -44,8 +44,8 @@ st.subheader("Dibuja el digito en el panel  y presiona  'Predecir'")
 # Specify canvas parameters in application
 drawing_mode = "freedraw"
 stroke_width = st.slider('Selecciona el ancho de línea', 1, 30, 15)
-stroke_color = '#000000' # Set background color to white
-bg_color = '#FFFFFF'
+stroke_color = '#FFFFFF' # Set background color to white
+bg_color = '#000000'
 
 # Create a canvas component
 canvas_result = st_canvas(
